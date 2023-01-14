7401a1d3019c223b45a2873309351389d739ad06
viju-dev <vijaydorugade20@gmail.com>
2023-01-14 21:17:28 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,7 +8,7 @@ public class Main {
         P.product(1,2);
     }
 
-    static class  Product{
+    public static class  Product{
         public int product(int x, int y) {
         return 0;
         }
