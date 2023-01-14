7088d6d39f77761d232cbd0c8a8a904f9ab3b1b8
viju-dev <vijaydorugade20@gmail.com>
2023-01-14 21:16:18 +0530

    files are uploaded to repo

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product P = new Product();
+        P.product(1,2);
+        P.product(1,2,3);
+        P.product(1,2);
+    }
 
+    static class  Product{
+        public int product(int x, int y) {
+        return 0;
+        }
+
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+        public double product(double x, double y) {
+            return 0;
+        }
+    }
 }
